2fe493773589a90d3a545a1799e1b46c2b676ca8
a.konnov <a.konnov@corpwebgames.com>
2020-01-13 17:31:53 +0300

    WIP

---
+++ New Unity Project/Assets/CurvedTextEditor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(scr))] 
+public class CurvedTextEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector ();
+	}
+}
