09c5ba9ae67401eed34013dbaa6c973f63ac598d
Aleksey Konnov <a.konnov@perfectplaystudio.com>
2020-10-25 23:49:44 +0300

    Змейка

---
+++ ML_UI_test/Assets/Scripts/SnakeMove.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMove : MonoBehaviour
+{
+    private Vector2Int gridPositon;
+    private float gridMoveTimer;
+    private float gridMoveTimerMax;
+    private Vector2Int gridMoveDirection;
+
+    private void Awake()
+    {
+        gridPositon = new Vector2Int(0, 0);
+        gridMoveTimerMax = 0.5f;
+        gridMoveTimer = gridMoveTimerMax;
+        gridMoveDirection = new Vector2Int(1, 0);
+    }
+
+    private void Update()
+    {
+        HandleIput();
+        HandleMovement();
+    }
+
+    private void HandleIput()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (gridMoveDirection.y != -1)
+            {
+                gridMoveDirection.x = 0;
+                gridMoveDirection.y = +1;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (gridMoveDirection.y != +1)
+            {
+                gridMoveDirection.x = 0;
+                gridMoveDirection.y = -1;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (gridMoveDirection.x != +1)
+            {
+                gridMoveDirection.x = -1;
+                gridMoveDirection.y = 0;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (gridMoveDirection.x != -1)
+            {
+                gridMoveDirection.x = +1;
+                gridMoveDirection.y = 0;
+            }
+        }
+    }
+
+    private void HandleMovement()
+    {
+        gridMoveTimer += Time.deltaTime;
+        if (gridMoveTimer >= gridMoveTimerMax)
+        {
+            gridPositon += gridMoveDirection;
+            gridMoveTimer -= gridMoveTimerMax;
+            transform.position = new Vector3(gridPositon.x, gridPositon.y);
+        }
+        
+    }
+}
