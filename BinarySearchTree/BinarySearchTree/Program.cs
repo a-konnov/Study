f8970b40feccb2d35a5b7d4cbce6a602a75d41fd
a.konnov <a.konnov@perfectplaystudio.com>
2020-12-17 12:32:15 +0300

    Заменил конструкцию switch на условия if.  Кодстиль

--- BinarySearchTree/BinarySearchTree/Program.cs
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace BinarySearchTree {
     internal class Program {
@@ -18,31 +17,35 @@ namespace BinarySearchTree {
                 if (val.CompareTo(this.value) < 0) {
                     if (this.left == null) {
                         this.left = new BinaryTree<T>(val, this);
-                    } else if (this.left != null)
+                    } 
+                    else if (this.left != null) {
                         this.left.Add(val);
+                    }
                 }
                 else if (val.CompareTo(this.value) > 0) {
                     if (this.right == null) {
                         this.right = new BinaryTree<T>(val, this);
-                    } else if (this.right != null)
+                    } 
+                    else if (this.right != null) {
                         this.right.Add(val);
+                    }
                 } else {
                     this.value = val;
                 }
             }
 
             private BinaryTree<T> Search(BinaryTree<T> tree, T val) {
-                if (tree == null)
+                if (tree == null) {
                     return null;
-                switch (val.CompareTo(tree.value)) {
-                    case 1:
-                        return Search(tree.right, val);
-                    case -1:
-                        return Search(tree.left, val);
-                    case 0:
-                        return tree;
-                    default:
-                        return null;
+                }
+                var compareResult = val.CompareTo(tree.value);
+                if (compareResult > 0) {
+                    return Search(tree.right, val);
+                }
+                else if (compareResult < 0) {
+                    return Search(tree.left, val);
+                } else {
+                    return tree;
                 }
             }
 
