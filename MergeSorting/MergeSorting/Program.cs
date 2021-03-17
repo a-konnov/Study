8f02cf8919bb96f4704b05ea266ca5631f2f4dd8
Aleksei Konnov <a.konnov@perfectplaystudio.com>
2021-03-17 22:50:14 +0300

    Попробовал метод расширения для массива

--- MergeSorting/MergeSorting/Program.cs
+++ MergeSorting/MergeSorting/Program.cs
@@ -2,22 +2,29 @@
 
 namespace MergeSorting {
     class Program {
+        public static int[] a = new int[] { 3, 7, 8, 4, 5, 5, 1, 7, 8, 3, 9, 2 };
+        
         static void Main(string[] args) {
-            Console.WriteLine($"Not sorting \t{string.Join(", ",a)}");
-            Sorting(a);
-            Console.WriteLine($"Sorting \t{string.Join(", ",a)}");
-            Console.ReadLine();
+            Console.WriteLine($"Unsorted \t{string.Join(", ", a)}");
+            a.ArraySorting();
+            Console.WriteLine($"Sorted \t{string.Join(", ", a)}");
+            Console.ReadKey();
+        }
+    }
+
+    public static class ArrayExtension {
+        public static int[] _auxiliaryArray;
+        
+        public static void ArraySorting(this int[] array) {
+            Sorting(array);
         }
 
-        private static int[] a = new int[] {3,7,8,4,5,5,1,7,8,3,9,2};
-        private static int[] _auxiliaryArray;
-        
         private static void Sorting(int[] array) {
             _auxiliaryArray = new int[array.Length];
             SortingProcess(array, 0, array.Length - 1);
         }
 
-        private static void SortingProcess(int[] array, int low, int high) {
+        public static void SortingProcess(int[] array, int low, int high) {
             if (high <= low)
                 return;
             int middle = low + (high - low) / 2;
@@ -29,22 +36,22 @@ namespace MergeSorting {
         private static void Merge(int[] array, int low, int middle, int high) {
             int leftHalfIndex = low;
             int rightHalfIndex = middle + 1;
-            for (int i = low; i <= high ; i++) {
+            for (int i = low; i <= high; i++) {
                 _auxiliaryArray[i] = array[i];
             }
 
             for (int i = low; i <= high; i++) {
                 if (leftHalfIndex > middle) {
-                    a[i] = _auxiliaryArray[rightHalfIndex];
+                    array[i] = _auxiliaryArray[rightHalfIndex];
                     rightHalfIndex++;
                 } else if (rightHalfIndex > high) {
-                    a[i] = _auxiliaryArray[leftHalfIndex];
+                    array[i] = _auxiliaryArray[leftHalfIndex];
                     leftHalfIndex++;
                 } else if (_auxiliaryArray[rightHalfIndex] <= _auxiliaryArray[leftHalfIndex]) {
-                    a[i] = _auxiliaryArray[rightHalfIndex];
+                    array[i] = _auxiliaryArray[rightHalfIndex];
                     rightHalfIndex++;
                 } else {
-                    a[i] = _auxiliaryArray[leftHalfIndex];
+                    array[i] = _auxiliaryArray[leftHalfIndex];
                     leftHalfIndex++;
                 }
             }
