8592faf5bc9a3793b00972e70ecae063129ae90c
Aleksei Konnov <a.konnov@perfectplaystudio.com>
2021-04-21 22:33:04 +0300

    k-syndicate course

---
+++ k-syndicate course/Assets/CodeBase/Cameralogic/CameraFollow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace CodeBase.Cameralogic {
+    public class CameraFollow : MonoBehaviour {
+        public float RotationAngleX;
+        public float Distance;
+        public float OffsetY;
+
+        [SerializeField] private Transform _following;
+
+
+        private void LateUpdate() {
+            if (_following == null) {
+                return;
+            }
+            
+            Quaternion rotation = Quaternion.Euler(RotationAngleX, 0, 0);
+            var position = rotation * new Vector3(0, 0, -Distance) + FollowingPointPosition();
+
+            transform.rotation = rotation;
+            transform.position = position;
+        }
+
+        public void Follow(GameObject following) => 
+            _following = following.transform;
+
+        private Vector3 FollowingPointPosition() {
+            Vector3 followingPosition = _following.position;
+            followingPosition.y += OffsetY;
+            return followingPosition;
+        }
+    }
+}
---
+++ k-syndicate course/Assets/CodeBase/Services/Input/IInputService.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Services.Input {
+    public interface IInputService {
+        Vector2 Axis { get; }
+        bool IsAttackButtonUp();
+    }
+}
