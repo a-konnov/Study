b5b98d4ef3fa2f3bb25f4dd6b6734063a13280fb
Алексей Коннов <a.konnov@playrix.com>
2025-04-04 14:32:55 +0600

    Удалил ненужные проекты

--- k-syndicate course/Assets/CodeBase/Services/Input/MobileInputService.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-namespace CodeBase.Services.Input {
-    public class MobileInputService : InputService {
-        public override Vector2 Axis => SimpleInputAxis();
-    }
-}
